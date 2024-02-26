d6e31bc81d689b32293c9a00305524eb6d10e98b
AlanDoDo <859588407@qq.com>
2024-02-26 13:15:56 +0800

    Alan

--- itmk-base-parent/itmk-base-web/src/main/java/com/itmk/StuRoomApplication.java
+++ itmk-base-parent/itmk-base-web/src/main/java/com/itmk/StuRoomApplication.java
@@ -3,6 +3,7 @@ package com.itmk;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 启动文件
 @SpringBootApplication
 public class StuRoomApplication {
     public static void main(String[] args) {
